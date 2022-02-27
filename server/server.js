33209f21875956c7249353aeb586093b0a4c9b9c
Alexandra Baker <baker.alexandra88@gmail.com>
2022-02-27 14:09:46 +0100

    add dummy api in server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/quiz", (req, res) => {
+  res.json({
+    question: "Hi, how are you?",
+  });
+});
+
+app.post("/api/quiz", (req, res) => {
+  res.sendStatus(200);
+});
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started on http://localhost:${server.address().port}`);
 });
