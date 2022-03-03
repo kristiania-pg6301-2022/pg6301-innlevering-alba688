509f846779e4509512ce176c91395a8deea28198
Alexandra Baker <baker.alexandra88@gmail.com>
2022-03-03 10:57:10 +0100

    added links and installed rimraf to clean cache

--- client/quizApp.jsx
+++ client/quizApp.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export function QuestionPage() {
   return (
     <div>
       <h1>Question Page</h1>
+      <div>
+        <Link to={"/"}>Back</Link>
+      </div>
     </div>
   );
 }
@@ -12,6 +16,9 @@ export function FrontPage() {
   return (
     <div>
       <h1>Quiz Application</h1>
+      <div>
+        <Link to={"/question"}>Start quiz</Link>
+      </div>
     </div>
   );
 }
